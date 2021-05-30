88f2c0279b09a9f1d6f42cc11484918da36bceee
VincentLiu777 <rliu8@uw.edu>
2021-05-30 17:56:30 +0800

    solve the initial conflict

--- FinalProject/ui.R
+++ FinalProject/ui.R
@@ -4,11 +4,8 @@ library(shiny)
 shinyUI(fluidPage(
 
     # Application title
-<<<<<<< HEAD
-    titlePanel(""),
-=======
+
     titlePanel("Covid-19 data visualization and analysis"),
->>>>>>> efbb96bc87c506ea2ec0bd30fdb5f6f2102078c5
 
     # Sidebar with a slider input for number of bins
     sidebarLayout(
