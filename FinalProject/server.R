efcb51a2099c6b65ab37aaf0a433e958781ec325
Antzed <43461513+Antzed@users.noreply.github.com>
2021-05-30 23:11:13 +0800

    slight edit

--- FinalProject/server.R
+++ FinalProject/server.R
@@ -2,24 +2,6 @@ library(shiny)
 library(dplyr)
 library(ggplot2)
 
-#covidData <- read.csv("../raw data/owid-covid-data (1).csv")
-
-#covidData[is.na(covidData)] = 0
-
-#data <- covidData %>%
-   # filter(continent == "" | location =="United States" | location == "China" | 
-            # location == "Russia" | location == "Japan" | location == "Germany" |
-            # location == "Brazil" | location == "United Arab Emirates" |
-            # location == "United Kingdom") %>%
-#    select(location, date, total_cases, new_cases, total_deaths, new_deaths, people_fully_vaccinated) %>%
-  #  mutate(month = format(as.POSIXlt(date), "%m/%Y")) %>%
- #   group_by(location, month) %>%
-  #  summarise(total_cases = max(total_cases), 
-            #  new_cases = max(new_cases),
-             # total_deaths = max(total_deaths),
-             # new_deaths = max(new_deaths),
-            #  people_fully_vaccinated = max(people_fully_vaccinated))
-
 source("data_clean.R")
 
 
@@ -30,7 +12,7 @@ shinyServer(function(input, output) {
                             choices = unique(data$location),
                             selected = "World")
     })
-    vac <- reactive({
+    totalCases <- reactive({
         print(input$slider)
         data %>%
             filter(month == format(as.POSIXlt(input$slider), "%m/%Y")) %>%
@@ -38,7 +20,7 @@ shinyServer(function(input, output) {
             select(location, total_cases)
     })
     output$cases_Plot <- renderPlot({
-        ggplot(vac()) +
+        ggplot(totalCases()) +
           geom_point(aes(x = total_cases, y = location, color = location), stat = "identity")
     })
 })
