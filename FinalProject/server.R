2627c4277c9b0691bd310205681877bb51290feb
Antzed <43461513+Antzed@users.noreply.github.com>
2021-05-30 21:39:02 +0800

    attempting to write side bar panel function

--- FinalProject/server.R
+++ FinalProject/server.R
@@ -1,7 +1,30 @@
 library(shiny)
+library(dplyr)
+
+read.csv("raw data/owid-covid-data(1).csv")
 
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
+    
+    dataInput <- reactive({
+      getSymbols(input$symb, src = "yahoo",
+                 from = input$dates[1],
+                 to = input$dates[2],
+                 auto.assign = FALSE)
+    })
+  
+  
+    output$plot <- renderPlot({
+      data <- getSymbols(input$symb, src = "yahoo",
+                         from = input$dates[1],
+                         to = input$dates[2],
+                         auto.assign = FALSE)
+      
+      chartSeries(data, theme = chartTheme("white"),
+                  type = "line", log.scale = input$log, TA = NULL)
+    })
+  
+    
 
     output$distPlot <- renderPlot({
 
--- FinalProject/ui.R
+++ FinalProject/ui.R
@@ -1,4 +1,5 @@
 library(shiny)
+library(dplyr)
 
 # Define UI for application that draws a histogram
 shinyUI(fluidPage(
@@ -14,7 +15,7 @@ shinyUI(fluidPage(
 
         # Show a plot of the generated distribution
         mainPanel(
-            plotOutput("")
+            plotOutput("plot"),
         )
     )
 ))
